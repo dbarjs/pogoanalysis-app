e3c79647f02e81c7f0547df4adc3393859e1864d
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-09 15:12:37 -0300

    add AppBar component

---
+++ components/AppBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center p-4">
+    <logo></logo>
+    <navigation-bar></navigation-bar>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import NavigationBar from '~/components/NavigationBar.vue'
+export default {
+  components: {
+    Logo,
+    NavigationBar
+  }
+}
+</script>

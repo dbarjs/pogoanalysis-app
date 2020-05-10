6955a30ab0f01c258443b7d2ac4ff6887286b972
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-10 15:13:06 -0300

    add NavigationBar and Link components

---
+++ components/NavigationBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="flex justify-center">
+    <ul class="flex flex-col items-center justify-center">
+      <navigation-link to="/league/master">Great League</navigation-link>
+      <navigation-link to="/league/master">Ultra League</navigation-link>
+      <navigation-link to="/league/master">Master League</navigation-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import NavigationLink from '~/components/NavigationLink.vue'
+export default {
+  components: {
+    NavigationLink
+  }
+}
+</script>
---
+++ components/NavigationLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <li
+    class="transform scale-90 hover:scale-100 duration-300 hover:text-blue-500 transition-all ease-in-out"
+  >
+    <nuxt-link :to="to">
+      <slot></slot>
+    </nuxt-link>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: [String, Boolean],
+      default: false
+    }
+  }
+}
+</script>

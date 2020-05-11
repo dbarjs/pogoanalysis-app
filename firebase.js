d90779db255d5a84573ffd36751ad1aa529478f3
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-11 06:21:52 -0300

    Add 'pokemons' reference; Add firestore increment

--- firebase.js
+++ firebase.js
@@ -2,17 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import firebaseConfig from '~/firebaseConfig'
 
-console.log(firebaseConfig)
-
 // App
 const app = firebase.initializeApp(firebaseConfig)
 
 // Firestore
 export const database = app.firestore()
-export const leagueRefs = {
-  great: database.collection('great'),
-  ultra: database.collection('ultra'),
-  master: database.collection('master')
-}
+export const increment = firebase.firestore.FieldValue.increment(1)
+export const decrement = firebase.firestore.FieldValue.increment(-1)
+export const pokemonsRef = database.collection('pokemons')
 
 export default app

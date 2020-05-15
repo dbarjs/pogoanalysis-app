ad4ead5afae4065cbaf0feec30b5a8b7c002021f
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-15 06:15:03 -0300

    Create Icon component

---
+++ components/Icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <span>
+    <svg
+      :style="{ width: size + 'px', height: size + 'px' }"
+      viewBox="0 0 24 24"
+    >
+      <path class="fill-current" :d="svgPath" />
+    </svg>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    svgPath: {
+      default: '',
+      type: String
+    },
+    color: {
+      default: '#000000',
+      type: String
+    },
+    size: {
+      default: 24,
+      type: [String, Number]
+    }
+  },
+  computed: {
+    viewbox() {
+      return `0 0 ${this.size} ${this.size}`
+    }
+  }
+}
+</script>
+
+<style></style>

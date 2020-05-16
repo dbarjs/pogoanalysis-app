4d16fbda6aee7b113ad3cc55eab2544834b4f26e
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-16 06:18:44 -0300

    Add stylelint config file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  extends: ['stylelint-config-recommended'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen']
+      }
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'no-empty-source': null
+  }
 }

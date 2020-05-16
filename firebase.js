aa86c4b55217f47921e6cf71f0870b5a1ac8ae30
Eduardo Barros <joseeduardobarros@gmail.com>
2020-05-16 06:18:04 -0300

    firebase: export Timestamp and usageRef database

--- firebase.js
+++ firebase.js
@@ -9,6 +9,8 @@ const app = firebase.initializeApp(firebaseConfig)
 export const database = app.firestore()
 export const increment = firebase.firestore.FieldValue.increment(1)
 export const decrement = firebase.firestore.FieldValue.increment(-1)
+export const Timestamp = firebase.firestore.Timestamp
 export const pokemonsRef = database.collection('pokemons')
+export const usageRef = database.collection('usage')
 
 export default app
